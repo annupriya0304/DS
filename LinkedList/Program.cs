5653db73e48773aff0924faee1e5adc6ddeb233e
annpriya <annupriya0304@gmail.com>
2018-08-02 00:37:56 +0530

    wdfdf

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -4,14 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LinkedList
+namespace DS
 {
-    class Program
+    class LinkedList
     {
-        static void Main(string[] args)
+        Node head;
+
+        
+        internal class Node
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int data;
+            internal Node next;
+           internal Node(int d) { data = d; next = null; } // Constructor
+        }
+        
+        public static void main(String[] args)
+        {
+            /* Start with the empty list. */
+            LinkedList llist = new LinkedList();
+
+            llist.head = new Node(1);
+            Node second = new Node(2);
+            Node third = new Node(3);           
+
+            llist.head.next = second; // Link first node with the second node           
+            second.next = third; // Link second node with the third node
+           
         }
     }
+
+
 }
